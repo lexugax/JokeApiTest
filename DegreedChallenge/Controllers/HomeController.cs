0f44be3def73eca7fa7fced510550c7a8348c501
Jaime Lajarin <lajarin@outlook.com>
2020-02-02 19:22:49 -0800

    Added some comments.

--- DegreedChallenge/Controllers/HomeController.cs
+++ DegreedChallenge/Controllers/HomeController.cs
@@ -6,21 +6,37 @@ namespace DegreedChallenge.Controllers
 {
 	public class HomeController : Controller
 	{
+		/// <summary>
+		/// Index page.
+		/// </summary>
+		/// <returns></returns>
 		public IActionResult Index()
 		{
 			return View();
 		}
 
+		/// <summary>
+		/// Random joke page. Shows one joke every 10 seconds.
+		/// </summary>
+		/// <returns></returns>
 		public IActionResult Random()
 		{
 			return View();
 		}
 
+		/// <summary>
+		/// Search joke page. Lets you search for jokes which contain a keyword.
+		/// </summary>
+		/// <returns></returns>
 		public IActionResult Search()
 		{
 			return View();
 		}
 		
+		/// <summary>
+		/// Returns a random joke as a json object.
+		/// </summary>
+		/// <returns></returns>
 		public async Task<IActionResult> GetRandomJoke()
 		{
 			JokeApplicationServices jokeApi = new JokeApplicationServices();
@@ -34,6 +50,12 @@ namespace DegreedChallenge.Controllers
 			return StatusCode(500);
 		}
 
+		/// <summary>
+		/// Returns the result of a joke search as a json object.
+		/// </summary>
+		/// <param name="searchTerm">Keywork to search in jokes.</param>
+		/// <param name="page">Page to return if more than one page available.</param>
+		/// <returns></returns>
 		public async Task<IActionResult> SearchJokes(string searchTerm, int page = 1)
 		{
 			JokeApplicationServices jokeApi = new JokeApplicationServices();
